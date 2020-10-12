8ff733727fec30596689735ec812b8416e4dfb00
Seif ElDen Ehab <seif.eld.ehab@gmail.com>
2020-10-12 21:33:51 +0200

    added id as primary key....

--- Routers/UserRouter.js
+++ Routers/UserRouter.js
@@ -3,4 +3,17 @@ const User = require('../Models/User.js')
 const express = require('express')
 const router = new express.Router();
 
+router.post('/createUser',async(req,res)=>{
+try{
+    const user = await User.create({
+        id:"sdfgh",
+        name:"seif",
+        phone:"01006569774"
+    });
+}
+catch(e){
+
+}
+})
+
 module.exports = router;
