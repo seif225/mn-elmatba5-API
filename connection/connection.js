4a5357fcf5035a7fddc6b19adced118d185acc6d
Seif ElDen Ehab <seif.eld.ehab@gmail.com>
2020-10-13 00:02:25 +0200

    added auth

--- connection/connection.js
+++ connection/connection.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize')
-const connection = new Sequelize(process.env.CLEARDB_DATABASE_URL,{
+const connection = new Sequelize('heroku_c62be069c0c1251', 'ba7684c3b5c9fa', '07d880e6',{
     host: 'us-cdbr-east-02.cleardb.com',
     dialect: 'mysql'
   });
