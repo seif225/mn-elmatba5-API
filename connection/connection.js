0305f6a8b867b70ec806f50a2d32622f2d61732f
Seif ElDen Ehab <seif.eld.ehab@gmail.com>
2020-10-13 00:46:41 +0200

    fixed connection

--- connection/connection.js
+++ connection/connection.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize')
 const connection = new Sequelize('heroku_c62be069c0c1251', 'ba7684c3b5c9fa', '07d880e6',{
-    host: process.env.CLEARDB_DATABASE_URL,
+    host:'us-cdbr-east-02.cleardb.com',
     dialect: 'mysql'
   });
 module.exports=connection
