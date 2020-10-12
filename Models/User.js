873036a43d1f56bd05e6dcd71c86020f24e6dd20
Seif ElDen Ehab <seif.eld.ehab@gmail.com>
2020-10-13 00:42:52 +0200

    added auth

--- Models/User.js
+++ Models/User.js
@@ -1,32 +1,32 @@
 const connection = require('../connection/connection.js')
-const { Sequelize, DataTypes } = require('sequelize');
+const Sequelize=require('sequelize');
 const User = connection.define('User' , 
 {
 name : {
     allowNull:false,
-    type: DataTypes.STRING,
+    type: Sequelize.STRING,
 },
 id: {
     allowNull:false,
     unique : true,
-    type:DataTypes.STRING,
+    type:Sequelize.STRING,
     primaryKey:true
 },
 phone : {
     allowNull:false,
     unique:true,
-    type:DataTypes.INTEGER
+    type:Sequelize.INTEGER
 },
 subType:{
     defaultValue:1,
     allowNull:false,
-    type:DataTypes.INTEGER
+    type:Sequelize.INTEGER
 },
 bio:{
-    type:DataTypes.TEXT
+    type:Sequelize.TEXT
 },
 tokens:{
-    type:DataTypes.String,
+    type:Sequelize.ARRAY(Sequelize.STRING),
     allowNull:false
 }
 }
--- connection/connection.js
+++ connection/connection.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize')
 const connection = new Sequelize('heroku_c62be069c0c1251', 'ba7684c3b5c9fa', '07d880e6',{
-    host: 'us-cdbr-east-02.cleardb.com',
+    host: CLEARDB_DATABASE_URL,
     dialect: 'mysql'
   });
 module.exports=connection
