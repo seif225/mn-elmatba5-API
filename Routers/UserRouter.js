3b8fb5555812ec18500d739c415a133fb6a385b0
Seif ElDen Ehab <seif.eld.ehab@gmail.com>
2020-10-12 21:22:59 +0200

    added id as primary key

--- Routers/UserRouter.js
+++ Routers/UserRouter.js
@@ -1,10 +1,7 @@
 const connection = require('../connection/connection.js')
 const User = require('../Models/User.js')
-const express = 'express'
+const express = require('express')
 const app = express()
 const router = new app.Router();
-
-
-
 
 module.exports = router;
