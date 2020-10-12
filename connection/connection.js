ecfafe124f0274162c435af7e12c071ad673d3b3
Seif ElDen Ehab <seif.eld.ehab@gmail.com>
2020-10-13 00:55:11 +0200

    remove timestamps

--- connection/connection.js
+++ connection/connection.js
@@ -1,6 +1,9 @@
 const Sequelize = require('sequelize')
 const connection = new Sequelize('heroku_c62be069c0c1251', 'ba7684c3b5c9fa', '07d880e6',{
     host:'us-cdbr-east-02.cleardb.com',
-    dialect: 'mysql'
+    dialect: 'mysql',
+    define: {
+        timestamps: false
+      }
   });
 module.exports=connection
