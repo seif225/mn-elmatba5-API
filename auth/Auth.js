2f4ace524de768de4e3981cfbc286bd939892359
Seif ElDen Ehab <seif.eld.ehab@gmail.com>
2020-10-13 19:19:06 +0200

    addmeal end point created

--- auth/Auth.js
+++ auth/Auth.js
@@ -6,10 +6,9 @@ const auth = async(req,res,next)=>{
         const token = req.header('Authorization').replace('Bearer ' ,'');
         console.log('token: '+token)
         const decoded = await jwt.verify(token,process.env.JWT_SECRET)
-        //const user = await User.findOne({_id:decoded._id, 'tokens.token':token})
         const user = await User.findOne({
             where :{
-                token:decoded
+                tokens:decoded
             }
         }
         )
