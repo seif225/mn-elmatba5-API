0e1478e6485b5f1fea09c330a5e0851dcce5af77
Seif ElDen Ehab <seif.eld.ehab@gmail.com>
2020-10-24 12:52:43 +0200

    migrate to postgres

--- connection/connection.js
+++ connection/connection.js
@@ -1,10 +1,11 @@
 const Sequelize = require('sequelize')
 const connection = new Sequelize(
-  'yTLSQ1FA4T',
-   'yTLSQ1FA4T', 
-   'GQ4jlUCbS9',{
-    host:'remotemysql.com',
-    dialect: 'mysql',
+  'postgres://xdbsocbaxgogof:6b7b209aaa6d02fcc00e5c7e00ccb802006d1b49e2f6cc9fc1818ebf4307cdec@ec2-107-22-241-205.compute-1.amazonaws.com:5432/dai1oh9ms5fkb0',
+   
+   {
+    host:'ec2-107-22-241-205.compute-1.amazonaws.com',
+    port:5432,
+    dialect: 'postgres',
     define: {
         timestamps: true
       }
