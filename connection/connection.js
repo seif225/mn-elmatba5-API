b9e21ebcee111681208b2a875919316f667f7773
Seif ElDen Ehab <seif.eld.ehab@gmail.com>
2021-05-28 13:37:16 +0200

    added pg

--- connection/connection.js
+++ connection/connection.js
@@ -1,16 +1,14 @@
 const Sequelize = require('sequelize')
-// const connection = new Sequelize(
-//   'dai1oh9ms5fkb0',
-//   'xdbsocbaxgogof',
-//   '6b7b209aaa6d02fcc00e5c7e00ccb802006d1b49e2f6cc9fc1818ebf4307cdec',
-//    {
-//     host:'ec2-107-22-241-205.compute-1.amazonaws.com',
-//     port:5432,
-//     dialect: 'postgres',
-//     ssl :true,
-//     define: {
-//         timestamps: true
-//       }
-//   }); 
-  const connection = new Sequelize('postgres://xdbsocbaxgogof:6b7b209aaa6d02fcc00e5c7e00ccb802006d1b49e2f6cc9fc1818ebf4307cdec@ec2-107-22-241-205.compute-1.amazonaws.com:5432/dai1oh9ms5fkb0')
+const connection = new Sequelize(
+  'postgres://xdbsocbaxgogof:6b7b209aaa6d02fcc00e5c7e00ccb802006d1b49e2f6cc9fc1818ebf4307cdec@ec2-107-22-241-205.compute-1.amazonaws.com:5432/dai1oh9ms5fkb0',
+   
+   {
+    host:'ec2-107-22-241-205.compute-1.amazonaws.com',
+    port:5432,
+    dialect: 'postgres',
+    define: {
+        timestamps: true
+      }
+  }); 
+  
 module.exports=connection
